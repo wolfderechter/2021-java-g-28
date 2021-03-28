e953739e3e9d7a562438ded39783d24b42bbb324
Stef Boerjan <stef.boerjan@student.hogent.be>
2021-03-28 20:37:28 +0200

    Initial commit (file structure)

---
+++ src/main/StartUp.java
@@ -0,0 +1,13 @@
+package main;
+
+public class StartUp {
+    public static void main(String [] arg) {
+        new StartUp().run();
+    }
+
+    private void run() {
+       
+        
+    }
+    
+}
