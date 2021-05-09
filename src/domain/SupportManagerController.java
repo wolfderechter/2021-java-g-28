fff5af3af49b8a4112f02bec71983c3ecf7a5851
Stef Boerjan <stef.boerjan@student.hogent.be>
2021-05-09 19:06:21 +0200

    fix

--- src/domain/SupportManagerController.java
+++ src/domain/SupportManagerController.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -62,9 +63,9 @@ public class SupportManagerController extends Controller {
 	public void addReaction(String text) {
 		// nog te vervangen met ingelogde usernaam
 		ticket.addReaction(text, false, "Nathan Supp Test");
-		GenericDaoJpa.startTransaction();
-		ticketRepo.update(ticket);
-		GenericDaoJpa.commitTransaction();
+		dm.updateTicket(ticket);
+		
+		
 	}
 
 	// nodig voor lijst van contractTypes voor tableview van ContractTypePanel
@@ -95,7 +96,62 @@ public class SupportManagerController extends Controller {
 		li = li.stream().filter(t->this.selectedFilterTypes.contains(t.getType()))
 				.filter(t->this.selectedFilterStatusen.contains(t.getStatus()))
 				.collect(Collectors.toCollection(FXCollections::observableArrayList));
+		System.out.println(li);
 		return (ObservableList<ITicket>) (Object) li;
+	}
+	
+	@Override
+	//deze methode gaat de lijst filteren die in table van tickets wordt gestoken
+	public void addStatusFilterOnTickets(List<? extends TicketStatusEnum> added) {
+		this.selectedFilterStatusen.addAll(added);
+		
+	}
+
+	@Override
+	public void removeStatusFilterOnTickets(List<? extends TicketStatusEnum> removed) {
+		this.selectedFilterStatusen.removeAll(removed);
+	}
+
+	@Override
+	public void addTypeFilterOnTickets(List<? extends TicketTypeEnum> added) {
+		this.selectedFilterTypes.addAll(added);
+	}
+	
+	@Override
+	public void removeTypeFilterOnTickets(List<? extends TicketTypeEnum> removed) {
+		this.selectedFilterTypes.removeAll(removed);
+	}
+	
+	@Override
+	// bij het zetten van de ticket wordt de ticket in de editticketpanel geset
+	public void setTicket(int ticketNr) {
+		Ticket ticket = dm.getAllTickets().stream().filter(t->t.getTicketNr() == ticketNr).findFirst().orElse(null);
+		ticketSubject.firePropertyChange("ticket", this.ticket, ticket);
+		this.ticket = ticket;
+	}
+	
+	@Override
+	// als ticket gewijzigd wordt gaat de ticket in editticketpanel ook veranderd
+	// worden
+	public void addTicketListener(PropertyChangeListener pcl) {
+		ticketSubject.addPropertyChangeListener(pcl);
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener pcl) {
+		ticketSubject.removePropertyChangeListener(pcl);
+	}
+	
+	@Override
+	public List<String> getAllCompanyNames() {
+		List<String> li = dm.getAllCompanies().stream().map(Company::getCompanyName).collect(Collectors.toList());
+		return li;
+	}
+
+	@Override
+	public List<String> getContactPersonFromCompanyName(String companyName) {
+		List<ContactPerson> li = dm.getAllCompanies().stream().filter(c->c.getCompanyName() == companyName).map(c->c.getContactPersons()).findFirst().orElse(null);
+		List<String> liString = li.stream().map(c->c.getUser().getUserName()).collect(Collectors.toList());
+		return liString;
 	}
 
 }
