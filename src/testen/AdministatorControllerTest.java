f9a17d6f9b2186c0eb1c8073f73c5a7e9840267d
Nathan Tersago <nathan.tersago@student.hogent.be>
2021-05-19 22:37:39 +0200

    bug fixes

---
+++ src/testen/AdministatorControllerTest.java
@@ -0,0 +1,18 @@
+package testen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+@ExtendWith(MockitoExtension.class)
+class AdministatorControllerTest {
+	
+	
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
