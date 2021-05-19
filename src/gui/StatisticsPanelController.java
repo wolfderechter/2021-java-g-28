0261fd59a0d79ec4dcda2d44d79d3a1a1b3b1ca8
Nathan Tersago <nathan.tersago@student.hogent.be>
2021-05-19 19:07:04 +0200

    grafiek werkend

--- src/gui/StatisticsPanelController.java
+++ src/gui/StatisticsPanelController.java
@@ -11,24 +11,34 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
-public class StatisticsPanelController extends AnchorPane{
+public class StatisticsPanelController extends AnchorPane {
 	@FXML
-	private LineChart<Number,Number> lnChart;
+	private LineChart<String,Number> lnChart;
 	@FXML
 	private ChoiceBox<String> chbCompany;
 	@FXML
 	private Label lblTicketsAmount;
+	@FXML
+	private NumberAxis Axis;
+	@FXML
+	private CategoryAxis xAxis;
+	@FXML
+	private Label lblAverageTime;
 	
+
 	private SupportManagerController dc;
 	private ICompany selectedCompany;
-	
+
 	public StatisticsPanelController(Controller dc2) {
 		this.dc = (SupportManagerController) dc2;
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("StatisticsPanel.fxml"));
@@ -40,45 +50,37 @@ public class StatisticsPanelController extends AnchorPane{
 			throw new RuntimeException(ex);
 		}
 		chbCompany.setItems(FXCollections.observableArrayList(dc.getAllCompanyNames()));
+		
 		chbCompany.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-			  @Override
-			  public void changed(ObservableValue<? extends String> observableValue, String nameSelected, String oldvalue) {
-				  selectedCompany =(ICompany) dc.getCompanyByName(nameSelected);
-				  setDataChart();
-			  }
+			@Override
+			public void changed(ObservableValue<? extends String> observableValue, String nameSelected,
+					String oldvalue) {
+				
+				selectedCompany = (ICompany) dc.getCompanyByName(oldvalue);
+				setDataChart();
+			}
 		});
-		chbCompany.getSelectionModel().selectFirst();
-		setDataChart();
 	}
-	
-	public void setDataChart(){
-		lblTicketsAmount.setText(Long.toString(selectedCompany.getTickets().stream().count()));
-		//setting the chart
-		final NumberAxis xAxis = new NumberAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("ticket");
-        //creating the chart
-        lnChart =new LineChart<Number,Number>(xAxis,yAxis);
-        lnChart.setTitle("ticket resolve time");
-        //defining a series
-        XYChart.Series series = new XYChart.Series();
-        series.setName("Data company");
-        //populating the series with data
-        selectedCompany.getTickets().forEach(t->series.getData().add(new XYChart.Data(1, "gsegrs")));
-        series.getData().add(new XYChart.Data(2, 14));
-        series.getData().add(new XYChart.Data(3, 15));
-        series.getData().add(new XYChart.Data(4, 24));
-        series.getData().add(new XYChart.Data(5, 34));
-        series.getData().add(new XYChart.Data(6, 36));
-        series.getData().add(new XYChart.Data(7, 22));
-        series.getData().add(new XYChart.Data(8, 45));
-        series.getData().add(new XYChart.Data(9, 43));
-        series.getData().add(new XYChart.Data(10, 17));
-        series.getData().add(new XYChart.Data(11, 29));
-        series.getData().add(new XYChart.Data(12, 25));
-        lnChart.getData().add(series);
+
+	public void setDataChart() {
+		long amount = selectedCompany.getTickets().stream().count();
+		lblTicketsAmount.setText(Long.toString(amount));
+		int total = selectedCompany.getTickets().stream().map(t->t.getResolvedDate().compareTo(t.getDateCreation())).reduce(0, Integer::sum);
+		double avg = total / amount;
+		lblAverageTime.setText(Double.toString( avg) + " Days");
+		// setting the chart
+		// creating the chart
+		lnChart.setTitle("ticket resolve time");
+		lnChart.getXAxis().setLabel("Ticket Tijdsduur");
+		lnChart.getYAxis().setLabel("Duration in days");
+		// defining a series
+		XYChart.Series<String, Number> series = new Series<>();
+		
+		// populating the series with data
+		selectedCompany.getTickets()
+				.forEach(t -> series.getData().add(new XYChart.Data<String, Number>(t.getTitle(),
+						t.getResolvedDate().compareTo(t.getDateCreation()))));
+		lnChart.setData(FXCollections.observableArrayList(series));
 	}
-	
-	
 
 }
